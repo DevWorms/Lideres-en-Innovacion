a1ee526705b668253dc1155e487641f1259cb895
Valentin Granados <valefcfhc@gmail.com>
2016-09-27 19:57:24 -0500

    Initial Commit

---
+++ Líderes en Innovación/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Líderes en Innovación
+//
+//  Created by Emmanuel Valentín Granados López on 27/09/16.
+//  Copyright © 2016 Emmanuel Valentín Granados López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
