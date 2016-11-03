0ff02f951a4ee741ed53430c17c831f5cdd36465
Valentin Granados <valefcfhc@gmail.com>
2016-11-02 20:44:50 -0600

    conexion ws y cosillas

--- Líderes en Innovación/PreguntasTableViewController.swift
+++ Líderes en Innovación/PreguntasTableViewController.swift
@@ -9,15 +9,90 @@
 import UIKit
 
 class PreguntasTableViewController: UITableViewController {
+    
+    var task : URLSessionDataTask?
+    var pregunta = ""
+    var respuesta1 = ""
+    var respuesta2 = ""
+    var respuesta3 = ""
+    var respuesta4 = ""
+    var numPreg = 0
+    var numGlobal = 0
+    var siempreUno = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        
+        print("inicia numGlobal: \(self.numGlobal)")
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "http://luisdiazapps.com/Projects/Tech/preguntas_JSON.php")! as URL)
+        request.httpMethod = "GET"
+        
+        self.task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            if error != nil {
+                print("error Request =\(error)")
+                return
+            } else {
+                
+                //let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                //print("responseString = \(responseString)")
+                
+                do {
+                    let jsonWithObjectRoot = try JSONSerialization.jsonObject(with: data! , options: []) as! NSArray
+                    
+                    print("count= \(jsonWithObjectRoot.count)")
+                    
+                    self.numPreg = jsonWithObjectRoot.count
+                    
+                    //for valueJson in jsonWithObjectRoot { // jsonWithObjectRoot[0...]
+                        
+                        if let dictionary = jsonWithObjectRoot[ self.numGlobal ] as? [String: String] {
+                        
+                            //print(dictionary)
+                        
+                            if let preg = dictionary["pregunta"] {
+                                self.pregunta = preg
+                            }
+                        
+                            if let ans_1 = dictionary["ans_1"] {
+                                self.respuesta1 = ans_1
+                            }
+                        
+                            if let ans_2 = dictionary["ans_2"] {
+                                self.respuesta2 = ans_2
+                            }
+                        
+                            if let ans_3 = dictionary["ans_3"] {
+                                self.respuesta3 = ans_3
+                            }
+                        
+                            if let ans_4 = dictionary["ans_4"] {
+                                self.respuesta4 = ans_4
+                            }
+                        
+                            //for (key, value) in dictionary {
+                                // access all key / value pairs in dictionary
+                                //print("\(key) , \(value)")
+                            //}
+                        }
+                    //}
+                    
+                    
+                } catch {
+                    print("error serializing JSON: \(error)")
+                }
+                
+            }
+            
+            self.siempreUno = 1
+            self.tableView.reloadData()
+            
+        }
+        self.task!.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,19 +104,20 @@ class PreguntasTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 3
+        return self.siempreUno
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 2
+        return 4
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellRespuesta", for: indexPath)
 
-        // Configure the cell...
+        let resp1 = cell.viewWithTag(1) as! UILabel
+        resp1.text = self.respuesta1
 
         return cell
     }
@@ -49,53 +125,35 @@ class PreguntasTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         //https://www.ralfebert.de/tutorials/ios-swift-uitableviewcontroller/
         
-        return "Section \(section)"
+        return self.pregunta
     }
     
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "bucle" {
+            let destinationViewController = segue.destination as! PreguntasTableViewController
+            
+            if self.numGlobal == self.numPreg-1 {
+                self.dismiss(animated: true, completion: nil)
+            }
+            
+            // pass data to destinationViewController
+            destinationViewController.numGlobal = self.numGlobal + 1
+        }
     }
-    */
+    
+    // cancela o deja asar el segue
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        return true
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 
 }
